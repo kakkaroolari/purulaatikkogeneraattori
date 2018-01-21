952770ace5021f674408a671e262779399acb22d
fivanl <ville.lehtisaari@sweco.fi>
2018-01-21 23:42:57 +0200

    Roof still not quite complete

--- python-proto/geometry.py
+++ python-proto/geometry.py
@@ -7,6 +7,7 @@ import itertools #import izip
 import re
 import json
 from constantdict import ConstantDict
+import math
 
 class Rotation(ConstantDict):
     """Tekla Position.Rotation"""
@@ -53,7 +54,17 @@ def parse_width(profile):
     trace("profile width: " + width)
     return float(width)
 
-def write_out(grid_x, grid_y, sockleProfile, footingProfile):
+def generate_loop(grid_x, grid_y, pairs):
+    master_polygon = []
+    xx = toDistances(grid_x)
+    yy = toDistances(grid_y)
+    for x_ind,y_ind in pairs:
+        # zero level z
+        edge = Point(xx[x_ind], yy[y_ind], 0.0)
+        master_polygon.append(edge)
+    return master_polygon
+
+def write_out(grid_x, grid_y, sockleProfile, footingProfile, centerline, roof_angle):
     # define line, or grid intersect
     pairs = [
         (0,1),
@@ -66,14 +77,17 @@ def write_out(grid_x, grid_y, sockleProfile, footingProfile):
         (0,2),
         (0,1)
     ]
-    master_polygon = []
-    xx = toDistances(grid_x)
-    yy = toDistances(grid_y)
-    for x_ind,y_ind in pairs:
-        # zero level z
-        edge = Point(xx[x_ind], yy[y_ind], 0.0)
-        master_polygon.append(edge)
-    
+    master_polygon = generate_loop(grid_x, grid_y, pairs)
+
+    roof_pairs = [
+        (0,1),
+        (3,1),
+        (3,2),
+        (1,2),
+        (0,1)
+    ]
+    roof_polygon = generate_loop(grid_x, grid_y, roof_pairs)
+
     trace("centroid is: ", centroid(master_polygon))
     #trace("sockle is: ", ', '.join([str(x) for x in polygon]))
     # TODO: make the layers bottom up and increase z offset
@@ -85,6 +99,8 @@ def write_out(grid_x, grid_y, sockleProfile, footingProfile):
     lower_reach = generate_lower_reach(master_polygon, 1000.0)
     higher_reach = generate_lower_reach(master_polygon, 4750.0)
     wall_studs = generate_wall_studs(master_polygon, 1000.0, 3650)
+    # bit different
+    roof_woods = generate_roof_studs(roof_polygon, 4750.0, centerline, roof_angle)
 
     #trace("high: " + pprint.pformat(higher_reach))
     #trace("studs: " + pprint.pformat(wall_studs))
@@ -100,7 +116,8 @@ def write_out(grid_x, grid_y, sockleProfile, footingProfile):
             named_section("sockle", sockle),
             named_section("lower_reach", lower_reach),
             named_section("higher_reach", higher_reach),
-            named_section("wall_studs", wall_studs)
+            named_section("wall_studs", wall_studs),
+            named_section("roof_woods", roof_woods)
         ], jsonfile, cls=MyEncoder)
         #jsonfile.write(pprint.pformat(combined_data))
 
@@ -114,13 +131,46 @@ def generate_lower_reach(polygon, z_offset):
 def create_wood_at(point, height, profile, rotation=None):
     low_point = point.Clone()
     high_point = low_point.Clone()
-    high_point.Translate(0,0,height)
+    high_point.Translate(height)
     return {
         "profile": profile,
         "rotation": rotation,
         "points": [low_point, high_point],
         "material": "Timber_Undefined"
     }
+
+def generate_roof_studs(master_polygon, z_offset, centerline, roof_angle):
+    overscan = 600.0 # negative to expand
+    roof_polygon = extend_or_subtract(master_polygon, -overscan, z_offset)
+    # hardcoded as rectangle
+    begin = roof_polygon[0]
+    end = roof_polygon[1]
+    direction = begin.GetVectorTo(end)
+    length = begin.distFrom(end)
+    holppa = overscan+125.0
+    count = int((length-2*holppa)/900.0)
+    half_width = 3600.0 #roof_polygon[-2].distFrom(roof_polygon[-1])
+    roofelevation = half_width*math.cos(math.radians(roof_angle))
+    trace("halflife: ", half_width, " dist: ", length)
+    # todo: much same as wall panel framing
+    #tuned_start = begin.clone()
+    # roof truss 5x2's
+    current = begin.Clone()
+    towards = Point.Normalize(direction, holppa)
+    current.Translate(towards)
+    roofpoints = []
+    rotation = Rotation.FRONT
+    roofpoints.append(create_wood_at(current, Point(0,half_width,roofelevation), "50*125", rotation))
+    towards = Point.Normalize(direction, 900.0)
+    #trace("start: {0}, end:{1}, direction: {2}".format(start, end, direction))
+    for i in range(count):
+        #trace("counting: ", i)
+        current.Translate(towards)
+        roofpoints.append(create_wood_at(current, Point(0,half_width,roofelevation), "50*125", rotation))
+        #another = current.Clone()
+        #another.x = end.x
+        #roofpoints.append(create_wood_at(another, Point(0,half_width,roofelevation), "50*125", rotation))
+    return roofpoints
 
 def generate_wall_studs(polygon, z_offset, height):
     # todo: purulaatikko constant
@@ -140,11 +190,9 @@ def generate_wall_studs(polygon, z_offset, height):
         tuned_start = start.Clone()
         towards = Point.Normalize(direction, -50)
         tuned_start.Translate(towards)
-        studpoints.append(create_wood_at(tuned_start, height, "100*100", rotation))
+        studpoints.append(create_wood_at(tuned_start, Point(0,0,height), "100*100", rotation))
         #studpoints.append(create_wood_at(end, height, "100*100"))
         # normal 4x2's
-		#pitkaseina middle.top.left.. eh ts paskaa
-		#paatyseina Ver.Rot.Hor=down,back, middle
         current = start.Clone()
         towards = Point.Normalize(direction, -50)
         current.Translate(towards)
@@ -153,10 +201,33 @@ def generate_wall_studs(polygon, z_offset, height):
         for i in range(count):
             #trace("counting: ", i)
             current.Translate(towards)
-            # todo: orientation(?)
-            studpoints.append(create_wood_at(current, height, "50*100", rotation))
+            studpoints.append(create_wood_at(current, Point(0,0,height), "50*100", rotation))
     return studpoints
 
+def point_grid(startpoint, direction, count, first_offset, create_first):
+    # UH, frakkit. needs running two points along...not height to reuse
+    # todo: refactor
+    grid = []
+    tuned_start = start.Clone()
+    towards = Point.Normalize(direction, -50)
+    tuned_start.Translate(towards)
+    #### studpoints.append(create_wood_at(tuned_start, height, "100*100", rotation))
+    if create_first is not None:
+        grid.append
+    #studpoints.append(create_wood_at(end, height, "100*100"))
+    # normal 4x2's
+    current = start.Clone()
+    towards = Point.Normalize(direction, -50)
+    current.Translate(towards)
+    towards = Point.Normalize(direction, 600)
+    #trace("start: {0}, end:{1}, direction: {2}".format(start, end, direction))
+    for i in range(count):
+        #trace("counting: ", i)
+        current.Translate(towards)
+        grid.append(current.Clone())
+    return grid
+
+    
 def direction_to_rotation(direction):
     if abs(direction.y) > abs(direction.x):
         if direction.y > 0:
@@ -216,18 +287,23 @@ def generate_offsetted_beams(foundationPolygon, profile, xy_offset, z_offset, ma
         })
     return beams
     
-def generate_offsetted_lines(master_polygon, xy_offset, z_offset, adjustByProfile=None):
-    mass_center = centroid(master_polygon)
+def extend_or_subtract(polygon, xy_offset, z_offset):
+    mass_center = centroid(polygon)
     footingCenter = []
 	#if instanceof(profile, float):
 	#	h_offset = profile
 	#else
 	#	h_offset = parse_width(profile)/2
-    for node in master_polygon:
+    for node in polygon:
         # todo: parse from profile
         endpoint = node.moveCloserTo(mass_center, xy_offset)
         endpoint.Translate(0, 0, z_offset)
         footingCenter.append(endpoint)
+    return footingCenter
+
+def generate_offsetted_lines(master_polygon, xy_offset, z_offset, adjustByProfile=None):
+    # move points i.e. 50mm closer to structure center (to 100*100 beam centerline)
+    footingCenter = extend_or_subtract(master_polygon, xy_offset, z_offset)
     footingLines = pairwise(footingCenter)
     polygonMidpoints = []
     adjustEndPoints = xy_offset
@@ -277,8 +353,9 @@ if __name__ == "__main__":
     
     grid_x = [0.00, 750.00, 3300.00, 5060.00]
     grid_y = [0.00, 1660.00, 7600.00]
+    centerline = [Point(0, 1660.0+7600.0/2, 0), Point(sum(grid_x), 1660.0+7600.0/2, 0)]
     sockle = "800*200"
     footing = "200*500"
 
     #trace("CONVERTED:\n" + pprint.pformat(attr_dict))
-    write_out(grid_x, grid_y, sockle, footing)
+    write_out(grid_x, grid_y, sockle, footing, centerline, 36.64)
