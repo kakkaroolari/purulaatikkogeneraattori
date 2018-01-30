5101f4d2a906b6c41aa49aafe4144bc2ebd0ffb7
fivanl <ville.lehtisaari@sweco.fi>
2018-01-30 22:49:00 +0200

    rotation matrix trials gone tho shait

--- python-proto/helpers.py
+++ python-proto/helpers.py
@@ -1,7 +1,9 @@
 import math
 import sys
 from constantdict import ConstantDict
-from transformations import reflection_matrix
+from transformations import (translation_matrix,
+                             rotation_matrix,
+                             angle_between_vectors)
 from point import Point
 import numpy as np
 
@@ -54,13 +56,32 @@ class CoordinateSystem(object):
         return self._normal.ToArr()
 
 def convert_points(points, coordinate_system):
-    transform_matrix = reflection_matrix(coordinate_system.origin(), coordinate_system.normal())
+    vector1 = [0,0,50]
+    vector2 = coordinate_system.normal()
+    # rotation
+    angle = angle_between_vectors(vector1, vector2, True)
+    #matrix1 = rotation_matrix(angle, -1*np.cross(vector2, vector1)).T
+    matrix1 = rotation_matrix(angle, np.cross(vector1, vector2)).T
+    # translate
+    matrix2 = translation_matrix(-1*coordinate_system.origin())
+    #transform_matrix = projection_matrix([1000,9620,1000], coordinate_system.normal())
     trace("cp: ", coordinate_system.origin(), " n: ", coordinate_system.normal())
     converted = []
     for point in points:
-        data = [point.x, point.y, point.z, 1]
+        #data = [point.x, point.y, point.z, 1]
+        data = [point.x, point.y, point.z]
         trace("dd: ", data)
-        temp = np.dot(transform_matrix, data)
-        pp = Point(temp[0], temp[1], temp[2])
-        converted.append(pp)
-        trace("conv: ", pp)
+        #temp = transform_matrix.dot(data)
+        #pp = Point(temp[0], temp[1], temp[2])
+        #converted.append(pp)
+        data = apply_transforms(matrix1, data)
+        data = apply_transforms(matrix2, data)
+        converted.append(data)
+        trace("conv: ", data)
+
+def apply_transforms(transform_matrix, point):
+    """Get vert coords in world space"""
+    m = np.array(transform_matrix)    
+    mat = m[:3, :3].T # rotates backwards without T
+    loc = m[:3, 3]
+    return point @ mat + loc
