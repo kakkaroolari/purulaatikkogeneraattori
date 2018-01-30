transforming but wrong axis flip

python-proto/helpers.py
@@ -1,9 +1,9 @@
 import math
 import sys
 from constantdict import ConstantDict
-import transformations
+from transformations import reflection_matrix
 from point import Point
-import numpy
+import numpy as np
 
 
 class Rotation(ConstantDict):
@@ -44,21 +44,23 @@ def get_part_data(profile, rotation, points, material, ts_class=None):
 
 class CoordinateSystem(object):
     def __init__( self, origin, normal ):
-        self.origin = origin
-        self.normal = normal
+        self._origin = origin
+        self._normal = normal
 
     def origin(self):
-        return self.origin.ToArr()
+        return self._origin.ToArr()
 
     def normal(self):
-        return self.normal.ToArr()
+        return self._normal.ToArr()
 
 def convert_points(points, coordinate_system):
     transform_matrix = reflection_matrix(coordinate_system.origin(), coordinate_system.normal())
+    trace("cp: ", coordinate_system.origin(), " n: ", coordinate_system.normal())
     converted = []
     for point in points:
-        data = point.ToArr()
-        temp = np.dot(transform_matrix, data.T).T
+        data = [point.x, point.y, point.z, 1]
+        trace("dd: ", data)
+        temp = np.dot(transform_matrix, data)
         pp = Point(temp[0], temp[1], temp[2])
-        converted.append()
+        converted.append(pp)
         trace("conv: ", pp)

python-proto/point.py
@@ -167,7 +167,7 @@ class Point( object ):
     # ---------------------- numpy wrap ----------------------
     def seg_isect_wrap(a, b, n, m):
         isect = seg_intersect(a.ToArr(), b.ToArr(), n.ToArr(), m.ToArr())
-        print("np.arr:", isect)
+        #print("np.arr:", isect)
         return Point(isect[0], isect[1], isect[2])
 
     def ToList(self):
@@ -218,7 +218,7 @@ class Point( object ):
     # yet another intersect
     def isect_line_plane_v3_wrap(p0, p1, p_co, p_no):
         isect = isect_line_plane_v3(p0.ToList(), p1.ToList(), p_co.ToList(), p_no.ToList())
-        print("np.arr:", isect)
+        #print("np.arr:", isect)
         return Point(isect[0], isect[1], isect[2])
 
 # intersection function